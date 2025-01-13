1ead028cf9d1fedd1f987115785934396d51f5b9
Thibaut MICETIC <thibaut.micetic@orange.fr>
2025-01-13 11:02:49 +0100

    appBar dynamique

--- lib/AppBar/appbar.dart
+++ lib/AppBar/appbar.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
 
+class MainAppBar extends StatefulWidget implements PreferredSizeWidget {
+  const MainAppBar(
+      {super.key,
+      required this.title,
+      required this.actions,
+      this.bgColor = Colors.green});
+
+  @override
+  State<MainAppBar> createState() => _MainAppBarState();
+
+  final String title;
+  final List<Widget> actions;
+  final Color bgColor;
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _MainAppBarState extends State<MainAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: widget.bgColor,
+      title: Text(widget.title),
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.search),
+        ),
+        ...widget.actions,
+      ],
+    );
+  }
+}
